e67f4e7ddf1ce3d555f881aa6f85af2d4ee9e3bc
Abel Isai Balam Balam <abel@etecno.com.mx>
2023-09-18 18:22:33 -0600

    add database port

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,12 +3,14 @@ import {
   DATABASE_NAME,
   DATABASE_HOST,
   DATABASE_PASSWORD,
-  DATABASE_USER
+  DATABASE_USER,
+  DATABASE_PORT
 } from '@/config'
 
 export const sequelize = new Sequelize({
   database: DATABASE_NAME,
   host: DATABASE_HOST,
+  port: DATABASE_PORT,
   password: DATABASE_PASSWORD,
   username: DATABASE_USER,
   dialect: 'mysql',
