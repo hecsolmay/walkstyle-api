5261eb2515ceedf58c1768d5d5789f066bdb45cd
hecsolmay <hecsolmay@gmail.com>
2023-10-30 13:40:12 -0600

    remove middlewares

--- src/routes/sales.routes.ts
+++ src/routes/sales.routes.ts
@@ -1,17 +1,17 @@
 import { Router } from 'express'
 import * as saleCtrl from '@/controllers/sales.controller'
-import { verifyAccessToken } from '@/middlewares/token'
-import { adminRoleValidator, existedUserValidator } from '@/middlewares/auth'
+// import { verifyAccessToken } from '@/middlewares/token'
+// import { adminRoleValidator, existedUserValidator } from '@/middlewares/auth'
 
 const router = Router()
 
-const middleWares = [verifyAccessToken, existedUserValidator]
-const adminMiddlewares = [...middleWares, adminRoleValidator]
+// const middleWares = [verifyAccessToken, existedUserValidator]
+// const adminMiddlewares = [...middleWares, adminRoleValidator]
 
-router.get('/', [...adminMiddlewares], saleCtrl.getSales)
+router.get('/', saleCtrl.getSales)
 
-router.get('/:saleId', [...adminMiddlewares], saleCtrl.getSaleById)
+router.get('/:saleId', saleCtrl.getSaleById)
 
-router.post('/', [...middleWares], saleCtrl.createSale)
+router.post('/', saleCtrl.createSale)
 
 export default router
